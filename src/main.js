2c4c9a5ecb4a123c55c7796368df74498bd2f625
ryanking13 <def6488@gmail.com>
2021-12-25 16:35:17 +0900

    Fix placeholder text

--- src/main.js
+++ src/main.js
@@ -16,8 +16,7 @@ x = {  'a':37,'b':42,
 x = 123456789.123456789E123456789
 
 if very_long_variable_name is not None and \
-    very_long_variable_name.field > 0 or \
-    very_long_variable_name.is_debug:
+    very_long_variable_name.field > 0:
     z = 'hello '+'world'
 else:
     world = 'world'
