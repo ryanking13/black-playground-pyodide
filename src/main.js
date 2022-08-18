68bb83fc1f0709089b7d8716b85fd9b47e95c755
Gyeongjae Choi <def6488@gmail.com>
2022-08-18 09:18:13 +0900

    Update Pyodide version

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.svelte';
 const app = new App({
   target: document.body,
   props: {
-    pyodideIndexUrl: 'https://cdn.jsdelivr.net/pyodide/v0.18.1/full/',
+    pyodideIndexUrl: 'https://cdn.jsdelivr.net/pyodide/v0.21.0/full/',
     githubProjectUrl: 'https://github.com/ryanking13/black-playground',
     blackWheel: 'black-21.10b1.dev16+gb3f3678-py3-none-any.whl',
     placeholderCode: `# Black Playground powered by Pyodide!
